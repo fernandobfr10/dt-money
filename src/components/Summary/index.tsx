a579835ea961df33407406931acd3ded49c14b0b
Fernando Junior <fernando.junior85@outlook.com>
2022-08-25 21:09:29 -0300

    feature: Created useSummary custom hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,27 +5,10 @@ import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 import { SummaryCard, SummaryContainer } from './styles'
 import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  )
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
